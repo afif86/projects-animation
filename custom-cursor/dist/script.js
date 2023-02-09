eb8d0eeafbb463b65f5bf82cec957b7d80b62ad7
afif86 <afif.hosseini86@gmail.com>
2023-02-09 22:44:29 +0200

    1

--- custom-cursor/dist/script.js
+++ custom-cursor/dist/script.js
@@ -1,16 +1,3 @@
-// const cursor = document.querySelector('.cursor');
-
-// document.addEventListener('mousemove', e => {
-//     cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
-// });
-
-// document.addEventListener('click', e => {
-//     cursor.classList.add("expand");
-//     setTimeout(() => {
-//         cursor.classList.remove("expand");
-//     }, 500);
-// });
-
 
 const cursor = document.querySelector('.cursor');
 
