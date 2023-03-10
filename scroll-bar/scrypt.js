1f42e4d4d0f1ae9a1e77be909b80324354419858
afif86 <afif.hosseini86@gmail.com>
2023-03-11 00:16:01 +0200

    typo

--- scroll-bar/scrypt.js
+++
@@ -1,9 +0,0 @@
-
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  let scrolled = (winScroll / height) * 100;
-  document.getElementById("progressBar").style.width = scrolled + "%";
-}
