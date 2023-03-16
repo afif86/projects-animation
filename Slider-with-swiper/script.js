72aa7a6404cc6eac4cdfcc5552ea742d960d30e0
afif86 <afif.hosseini86@gmail.com>
2023-03-17 00:17:35 +0200

    slider

--- Slider-with-swiper/script.js
+++ Slider-with-swiper/script.js
@@ -1,3 +1,7 @@
+document.addEventListener("keydown", (e) => {
+  NextToPagination(e);
+});
+
 const swiper = new Swiper('.swiper-container', {
   slideToClickedSlide: true,
   slidePerView: "auto",
@@ -40,35 +44,91 @@ const swiper = new Swiper('.swiper-container', {
     clickable: true,
   },
 
-  on: {
-    keyPress: function (e) {
-      
-      // to run just when it is active otherwise it will pass the navigation 
-      const elems = Array.from(document.querySelectorAll('.checking'))
-      if (elems.indexOf(document.activeElement) > -1) {
+});
 
-        // it will check if the other slides has button or not        
-        if (e.realIndex < e.slides.length) {
-          for (let i = e.realIndex; i < e.slides.length; i++){
-            if (i + 1 >= e.slides.length) {
-              break;
-            } else {
-                if (e.slides[i + 1].dataset.link) { 
-                  this.slideTo(i+1);
-                  return true;
-                }
-              }
-            
-          }
+
+const checkObject = {
+  indexOfSlidesWithLink: [],
+};
+
+swiper.on("reachEnd", () => {
+
+})
+
+
+swiper.slides.forEach((slide,i) => {
+  if (slide.dataset.link) {
+    checkObject.indexOfSlidesWithLink.push(i);
+  }
+  
+});
+
+
+const buttonTagInsideSlide = ".checking"
+
+const NextToPagination = (e) => {
+  function slideChanger(dir= "forward") {
+    function focusOnLink(i) { 
+        document.activeElement.blur()
+        swiper.slides[i].querySelector(buttonTagInsideSlide).focus()
+        if (document.activeElement !== swiper.slides[swiper.activeIndex].querySelector(buttonTagInsideSlide)){
+          focusOnLink(i)
+
         }
 
-       
-      }
-
+      return true
     }
+
+
+    const changeLogic = (i) => {
+      swiper.slideTo(i);
+      setTimeout(() => {
+        if (
+          document.activeElement !==
+          swiper.slides[swiper.activeIndex].querySelector(buttonTagInsideSlide)
+        ) {
+      
+          focusOnLink(swiper.activeIndex);
+        }
+      }, 400);
+      return true;
+    }
+
+    if(dir==="backward"){
+      for(let i = swiper.activeIndex; i >= 0; i--){
+        if(i-1 < -1){
+          return true;
+        }
+        if(checkObject.indexOfSlidesWithLink.includes(i-1)){
+          changeLogic(i-1);
+          return true;
+        }
+      }
+      return;
+
+    } if (dir === "forward") {
+      for (let i = swiper.activeIndex; i < swiper.slides.length; i++) {
+        if (checkObject.indexOfSlidesWithLink.includes(i+1)) {
+          changeLogic(i+1);
+          return true;
+        }
+      }
+      return true;
+    }
+      
+
   }
 
-});
+  if (swiper.pagination.bullets.includes(document.activeElement) || Array.from(document.querySelectorAll(buttonTagInsideSlide)).includes(document.activeElement)) {
+
+      if (e.shiftKey && e.code === "Tab") {
+        slideChanger("backward");
+      } else if (e.code === "Tab") {       
+        slideChanger();
+    
+      }
+  }
+};
 
 
 
